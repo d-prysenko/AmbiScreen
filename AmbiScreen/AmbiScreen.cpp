c8ab0d38cfc9aa6c9a296efdbb592d26cdc17d28
Dmitriy Prysenko <d.prysenko@ya.ru>
2023-03-08 22:40:27 +0300

    Initial commit

---
+++ AmbiScreen/AmbiScreen.cpp
@@ -0,0 +1,12 @@
+﻿// AmbiScreen.cpp : Defines the entry point for the application.
+//
+
+#include "AmbiScreen.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
