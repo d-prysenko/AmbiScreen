567b1b5a733a52cf1dfdfdcd8878f034e660f1a6
Dmitriy Prysenko <d.prysenko@ya.ru>
2023-04-11 09:35:50 +0300

    temp2

--- src/ScreenManager.h
+++ src/ScreenManager.h
@@ -1,18 +1,33 @@
 #pragma once
 #include <cstdint>
+#include <chrono>
+#include <exception>
 
-static class FrameSectionsBuffer
+#include <mutex>
+
+#include <ScreenCapture.h>
+#include "utils/Image.hpp"
+
+using SL::Screen_Capture::ICaptureConfiguration;
+using SL::Screen_Capture::ScreenCaptureCallback;
+using SL::Screen_Capture::Monitor;
+using SL::Screen_Capture::Image;
+
+enum CornerResolveMethod : uint8_t
 {
-public:
-	enum CornerResolveMethod : uint8_t
-	{
-		Split,
-		HalfIntersection,
-		Intersection,
-	};
+	Split,
+	HalfIntersection,
+	Intersection,
+};
 
-private:
+struct Pixel
+{
+	uint8_t R, G, B;
+};
 
+struct FrameSectionsBuffer
+{
+	Pixel *top, *bottom, *left, *right;
 };
 
 class ScreenManager
@@ -21,9 +36,25 @@ public:
 	
 
 private:
+
+	int width;
+	int height;
+
 	int hSectCount;
 	int vSectCount;
-	FrameSectionsBuffer::CornerResolveMethod cornerResolveMethod;
+
+	int hSectWidth;
+	int vSectWidth;
+	int sectDepth;
+
+	FrameSectionsBuffer sections;
+
+	CornerResolveMethod cornerResolveMethod;
+
+	std::shared_ptr<ICaptureConfiguration<ScreenCaptureCallback>> captureConfig = nullptr;
+
+	std::mutex buffer;
+
 
 public:
 	ScreenManager();
@@ -32,12 +63,25 @@ public:
 	/// <param name="vSectCount">Horizontal Sections count</param>
 	ScreenManager(int hSectCount, int vSectCount);
 
+	void startCapture();
+
 	ScreenManager setVerticalSectionsCount(int);
 	ScreenManager setHorizontalSectionsCount(int);
-	ScreenManager setCornerResolveMethod(FrameSectionsBuffer::CornerResolveMethod);
+	ScreenManager setCornerResolveMethod(CornerResolveMethod);
 	
 	bool isCapturing();
 
 	// std::condition_variable::wait
 	FrameSectionsBuffer getSections();
+
+private:
+	void checkPermissions();
+
+	std::shared_ptr<ICaptureConfiguration<ScreenCaptureCallback>> getCaptureConfig();
+	std::vector<Monitor> getMonitors();
+	void onNewFrame(const Image& img, const Monitor& monitor);
+
+	void getTopPixelLine(const SL::Screen_Capture::ImageBGRA* src, Pixel* top);
+	void getBottomPixelLine(const SL::Screen_Capture::ImageBGRA* src, Pixel* top);
+	void getHorizontalPixelLine(const SL::Screen_Capture::ImageBGRA* src, Pixel* buffer, int firstRow);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,14 @@
 ﻿#include <iostream>
-#include <chrono>
-#include <ScreenCapture.h>
 
-#include "utils/Image.hpp"
+#include "ScreenManager.h"
 
-#define TJE_IMPLEMENTATION
-#include "utils/tiny_jpeg.h"
+//const int VERTICAL_LEDS_COUNT = 30;
+const int HORIZONTAL_LEDS_COUNT = 20;
+#define VERTICAL_LEDS_COUNT 30
 
 
 int main()
 {
-    std::atomic<int> counter;
-    
-    std::cout << "Hello CMake." << std::endl;
-
-    std::srand(std::time(nullptr));
-    std::cout << "Starting Capture Demo/Test" << std::endl;
-    std::cout << "Testing captured monitor bounds check" << std::endl;
-
-    std::cout << "Checking for Permission to capture the screen" << std::endl;
-    if (SL::Screen_Capture::IsScreenCaptureEnabled()) {
-        std::cout << "Application Allowed to Capture the screen!" << std::endl;
-    }
-    else if (SL::Screen_Capture::CanRequestScreenCapture()) {
-        std::cout << "Application Not Allowed to Capture the screen. Waiting for permission " << std::endl;
-        while (!SL::Screen_Capture::IsScreenCaptureEnabled()) {
-            SL::Screen_Capture::RequestScreenCapture();
-            std::cout << " . ";
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-        }
-    }
-    else {
-        std::cout << "Cannot Capture the screen due to permission issues. Exiting" << std::endl;
-        return 0;
-    }
-
-    auto goodmonitors = SL::Screen_Capture::GetMonitors();
-    for (auto& m : goodmonitors) {
-        std::cout << m << std::endl;
-        assert(SL::Screen_Capture::isMonitorInsideBounds(goodmonitors, m));
-    }
-    auto badmonitors = SL::Screen_Capture::GetMonitors();
-
-    std::chrono::high_resolution_clock::time_point last = std::chrono::high_resolution_clock::now();
-    uint32_t frames = 0;
-
-    //Setup Screen Capture for all monitors
-    auto framgrabber = SL::Screen_Capture::CreateCaptureConfiguration([]() {
-        return SL::Screen_Capture::GetMonitors();
-    })->onNewFrame([&](const SL::Screen_Capture::Image& img, const SL::Screen_Capture::Monitor& monitor) {
-        frames++;
-
-        counter.fetch_add(1);
-
-        auto s = std::to_string(counter) + std::string("MONITORDIF_") + std::string(".jpg");
-        auto size = Width(img) * Height(img) * sizeof(SL::Screen_Capture::ImageBGRA);
-        auto imgbuffer(std::make_unique<unsigned char[]>(size));
-        Image::ExtractAndConvertToRGBA(img, imgbuffer.get(), size);
-        tje_encode_to_file(s.c_str(), Width(img), Height(img), 4, (const unsigned char*)imgbuffer.get());
-
-        //printf("Width: %d Height: %d\n", Width(img), Height(img));
-
-        if (frames % 40 == 0) {
-
-            std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
-            printf("fps: %f\n", 40.0f / std::chrono::duration_cast<std::chrono::duration<double>>(now - last).count());
-
-
-            last = now;
-        }
-    })->start_capturing();
-
-    framgrabber->setFrameChangeInterval(std::chrono::milliseconds(70));//100 ms
-    framgrabber->setMouseChangeInterval(std::chrono::milliseconds(70));//100 ms
-
-    std::this_thread::sleep_for(std::chrono::seconds(600));
 
     // Create manager
 
@@ -88,7 +22,9 @@ int main()
     //     usbManager.send(serialize(sections));
     // }
 
-
+    ScreenManager screenManager = ScreenManager(HORIZONTAL_LEDS_COUNT, VERTICAL_LEDS_COUNT);
+    
+    screenManager.startCapture();
 
 	return 0;
 }
